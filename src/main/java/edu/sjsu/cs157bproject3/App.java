8979cf64c82bf9a0c52c68a24a020774357320a5
Guillermo-C <ninjamemo@Guillermos-MacBook-Pro.local>
2017-04-12 13:26:09 -0700

    First commit

---
+++ src/main/java/edu/sjsu/cs157bproject3/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sjsu.cs157bproject3;
+
+/**
+ *
+ * @author ninjamemo
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.print("\nHello World!");
+    }
+}
