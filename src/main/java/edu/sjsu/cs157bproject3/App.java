346c9349b4409498fe2de7bb22e046c6fc25abff
Guillermo-C <ninjamemo@10.0.0.75>
2017-04-29 21:45:00 -0700

    Cleaned up this file and also reorganized the data in the database. The database now consists of dates which are the primary keys as instructed for this project.

--- src/main/java/edu/sjsu/cs157bproject3/App.java
+++ src/main/java/edu/sjsu/cs157bproject3/App.java
@@ -6,18 +6,32 @@
 package edu.sjsu.cs157bproject3;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TemporalType;
+import static javax.persistence.TemporalType.DATE;
+import javax.persistence.TypedQuery;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
@@ -28,17 +42,19 @@ import org.hibernate.service.ServiceRegistryBuilder;
  */
 public class App {
     
+    //  Ask for user input.
     Scanner input = new Scanner(System.in);
 
     //  Print the query results.
     public static void printQueryResults(Query query){
             List<?> list = query.list();
             SalesTransactions result;
-
+            System.out.println("\nResults:\n");
             for(int i = 0; i < list.size(); i++){
             result = (SalesTransactions)list.get(i);
             System.out.println(result);
             }
+            System.out.println("\nEnd of results!\n");
             
     }
     
@@ -68,12 +84,49 @@ public class App {
         return sale = new SalesTransactions(manualDate, ProductName, quantity, unit_cost);
     }
 
+    //  Look for records of a particular name
     public Query findProduct(Session session){
+        
         System.out.println("\nEnter ProductName: ");
         String ProductName = input.nextLine();
         Query query  = session.createQuery("FROM Sales WHERE ProductName = :ProductName");
         query.setParameter("ProductName", ProductName);
         
+        return query;
+    }
+    
+    //  Look for records within a time interval
+    public Query findProductWTimeInterval(Session session) throws IllegalArgumentException{
+        
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd"); 
+        Date dateX = new Date();
+        Date dateY = new Date();
+        
+        System.out.println("\nPlease enter the first date: ");
+        String date0 = input.nextLine();
+        try {
+            dateX = dateFormat.parse(date0);
+        } catch (ParseException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        System.out.println("\nPlease enter the second date: ");
+        String date1 = input.nextLine();
+        try {
+        dateY = dateFormat.parse(date1);
+        } catch (ParseException ex) {
+        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }     
+        
+        Query query = session.createQuery("SELECT s FROM Sales s WHERE s.date BETWEEN :date0 AND :date1");
+        query.setDate("date0", dateX);
+        query.setDate("date1", dateY);
+        
+        return query;
+    }
+    
+    public Query getAll(Session session){
+        Query query = session.createQuery("FROM Sales");
         return query;
     }
     
@@ -92,7 +145,7 @@ public class App {
         Transaction transaction = session.beginTransaction();
         
         //  Query for testing
-        Query query = session.createQuery("FROM Sales WHERE Quantity = 1");
+        //Query query = session.createQuery("FROM Sales WHERE Quantity = 1");
         
         //System.out.println("From the query results function");
         //printQueryResults(query);
@@ -103,11 +156,21 @@ public class App {
         //SalesTransactions newOne = app.createNewRecord();     
         //session.save(newOne);
         
-        //SalesTransactions something = new SalesTransactions("12/09/11", "something",2, 43.23);
+        //SalesTransactions something = new SalesTransactions("12/09/19", "Yes",2, 43.23);
+        
+        
+        /*EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("edu.sjsu_CS157BProject3_jar_1.0-SNAPSHOTPU"); 
+        EntityManager em = emfactory.createEntityManager(); 
+        Query queryx = app.findProductWTimeInterval(session, em);*/
         //session.save(something);
         
-        Query query2 = app.findProduct(session);
+        /*Query query2 = app.findProduct(session);
         printQueryResults(query2);
+
+        Query query3 = app.findProductWTimeInterval(session);
+        printQueryResults(query3);*/
+        
+
         
         transaction.commit();
         session.close();
